ddb1c4d681d1601e8929683efd911058f6484980
gabriellefrancine <202301133@iacademy.edu.ph>
2025-08-05 17:20:35 +0800

    Initial Commit

---
+++ src/form.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/src/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <script defer src="/public/assets/JavaScript/sidebar.js"></script>
+    <title>Student Admission</title>
+</head>
+<body>
+    <header class="header_container">
+        <img src="/Images/logo.svg" alt="iACADEMY Logo">
+    </header>
+
+    <div class="mainContainer">
+
+        <div class="content">
+
+        </div>
+    </div>
+</body>
+</html>
